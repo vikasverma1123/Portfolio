0250f412141017c376a11eec626200a3f11daa5f
Vikas Verma <vv4226426@gmail.com>
2024-06-29 18:26:32 +0530

    added main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Front-end Developer","Machine Learning Engineer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
